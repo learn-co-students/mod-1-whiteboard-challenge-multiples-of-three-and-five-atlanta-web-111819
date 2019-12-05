a09dacb4dd9520e59747225f8b83c32aa5cf6bb4
RobertTTaylor9517 <robt15@hotmail.com>
2019-12-05 09:35:34 -0500

    Done.

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,17 @@
-# Enter your object-oriented solution here!
+class Multiples
+
+    attr_reader :num
+
+    def initialize(num)
+        @num = num
+    end
+
+    def sum_multiples
+        collect_multiples.sum
+    end
+
+    def collect_multiples
+        init_array = (1..@num - 1)
+        init_array.select{|num| num % 3 == 0 || num % 5 == 0}
+    end
+end
